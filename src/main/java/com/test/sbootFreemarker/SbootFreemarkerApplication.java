816a5239e215680d89aa69bbaf127c2aedc3646d
chuckma <robbincen@163.com>
2018-03-20 16:52:27 +0800

    init

---
+++ src/main/java/com/test/sbootFreemarker/SbootFreemarkerApplication.java
@@ -0,0 +1,12 @@
+package com.test.sbootFreemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootFreemarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootFreemarkerApplication.class, args);
+	}
+}
